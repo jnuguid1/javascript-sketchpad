def5911b58901232bfde6c28c73bc6b740823792
Jared <jarednuguid@gmail.com>
2022-12-12 17:04:42 -0500

    Add variable grid size on button click

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const grid = document.querySelector('.grid');
-const GRID_WIDTH = 16;
+const newGridButton = document.querySelector('.new-grid-button');
+const DEFAULT_GRID_WIDTH = 16;
 
-for (let i = 0; i < GRID_WIDTH ** 2; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    gridSquare.addEventListener("mouseover", colorSquare)
-    grid.appendChild(gridSquare);
-}
+generateGrid(DEFAULT_GRID_WIDTH)
+
+newGridButton.addEventListener('click', generateNewGrid);
 
 function colorSquare(e) {
     e.target.style.backgroundColor = 'black';
+}
+
+function generateNewGrid() {
+    const newGridWidth = prompt('Enter the number of squares per side', 16);
+    if (newGridWidth <= 0) {
+        alert('Enter a number greater than 0');
+    } else {
+        while (grid.hasChildNodes()) {
+            grid.removeChild(grid.firstChild);
+        }
+        generateGrid(newGridWidth);
+    }   
+}
+
+function generateGrid(width) {
+    grid.style.width = width*10+'px';
+    for (let i = 0; i < width ** 2; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridSquare.addEventListener('mouseover', colorSquare);
+        grid.appendChild(gridSquare);
+    }
 }
