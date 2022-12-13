3c8e931cf383a2b60cd4fba007adb166ce1ae7df
Jared <jarednuguid@gmail.com>
2022-12-13 15:38:50 -0500

    Add working random color mode and initial default mode

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const grid = document.querySelector('.grid');
 const newGridButton = document.querySelector('.new-grid-button');
+const defaultColorButton = document.querySelector('.default-color-button');
+const randomColorButton = document.querySelector('.random-color-button');
+
 const DEFAULT_GRID_WIDTH = 500;
 const DEFAULT_GRID_DIMENSION = 16;
 const MAX_GRID_DIMENSION = 64;
 
-generateGrid(DEFAULT_GRID_DIMENSION)
+generateGrid(DEFAULT_GRID_DIMENSION);
 
 newGridButton.addEventListener('click', generateNewGrid);
+randomColorButton.addEventListener('click', setRandomMode);
 
-function colorSquare(e) {
+function colorSquareBlack(e) {
     e.target.style.backgroundColor = 'black';
+}
+
+function colorSquareRandom(e) {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 
 function generateNewGrid() {
@@ -33,7 +44,16 @@ function generateGrid(width) {
         gridSquare.classList.add('grid-square');
         gridSquare.style.width = gridSquareWidth;
         gridSquare.style.height = gridSquareWidth;
-        gridSquare.addEventListener('mouseover', colorSquare);
+        gridSquare.addEventListener('mouseover', colorSquareBlack);
         grid.appendChild(gridSquare);
+    }
+}
+
+function setRandomMode() {
+    const children = grid.children;
+    for (let i = 0; i < children.length; i++) {
+        const gridSquare = children[i];
+        gridSquare.removeEventListener('mouseover', colorSquareBlack);
+        gridSquare.addEventListener('mouseover', colorSquareRandom);
     }
 }
